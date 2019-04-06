06f24b1b49959c024b9055a4de86bc94e1eba019
Shelley Vohr <shelley.vohr@gmail.com>
2019-04-05 20:45:15 -0700

    fix commit fetch

--- entrypoint.js
+++ entrypoint.js
@@ -19,15 +19,17 @@ Toolkit.run(async tools => {
     const commits = await fetchUnreleasedCommits(branch)
     tools.log.info(`Found ${commits.length} commits on ${branch}`)
 
-    const response = `Unreleased commits in *${branch}*:\n${commits.map(c => {
-      `- \`<${c.html_url}|${c.sha.slice(0, 8)}>\` ${linkifyPRs(c.commit.message.split(/[\r\n]/)[0])}`
-    }).join('\n')}`
+    const response = `Unreleased commits in *${branch}*:\n${commits.map(c => `- \`<${c.html_url}|${c.sha.slice(0, 8)}>\` ${linkifyPRs(c.commit.message.split(/[\r\n]/)[0])}`).join('\n')}`
 
-    const result = await slackWebClient.chat.postMessage(JSON.parse(response))
-    if (result.ok === "true") {
+    const result = await slackWebClient.chat.postMessage({
+      channel: '#wg-releases',
+      text: response
+    })
+
+    if (result.ok) {
       tools.exit.success(`Audit message sent for ${branch} 🚀`)
     } else {
-      tools.exit.failure(`Unable to send audit info for ${branch}: ` + postResult.error)
+      tools.exit.failure(`Unable to send audit info for ${branch}: ` + result.error)
     }
   }
 }, {
@@ -42,12 +44,11 @@ async function getAll(urlEndpoint) {
 
 async function* getAllGenerator(urlEndpoint) {
   let next = urlEndpoint
-
   while (true) {
     const resp = await fetch(next, { 
       headers: { Authorization: `token ${GITHUB_TOKEN}` }
     })
-    
+
     if (!resp.ok) {
       if (resp.headers.get('x-ratelimit-remaining') === '0') {
         const resetTime = Math.round(resp.headers.get('x-ratelimit-reset') - Date.now() / 1000)
@@ -55,16 +56,14 @@ async function* getAllGenerator(urlEndpoint) {
       }
       throw new Error(`${resp.status} ${resp.statusText}`)
     }
-
+  
     const json = await resp.json()
     yield *json
+
     if (!resp.headers.get('link')) break
-    throw new Error(resp)
-    const next_link = resp.headers.get('link').split(/,/).map(x => { 
-      x.split(/;/)
-    }).find(x => {
-      x[1].includes('next')
-    })
+    const next_link = resp.headers.get('link').split(/,/)
+      .map(x => x.split(/;/))
+      .find(x => x[1].includes('next'))
 
     if (!next_link) break
     next = next_link[0].trim().slice(1, -1)
