2f7ba16253e6f80b4f70986f704e126276104f34
Shelley Vohr <shelley.vohr@gmail.com>
2019-04-05 22:08:58 -0700

    add now

--- entrypoint.js
+++ entrypoint.js
@@ -1,12 +1,8 @@
 const { Toolkit } = require('actions-toolkit')
 const { WebClient } = require('@slack/web-api')
-const fetch = require('node-fetch')
 
-const GH_API_PREFIX = 'https://api.github.com'
-const ORGANIZATION_NAME = 'electron'
-const REPO_NAME = 'electron'
+const { fetchUnreleasedCommits, linkifyPRs } = require('index.js')
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN
 
 const slackWebClient =  new WebClient(SLACK_BOT_TOKEN)
@@ -40,55 +36,3 @@ Toolkit.run(async tools => {
 }, {
   secrets: ['GITHUB_TOKEN', 'SLACK_BOT_TOKEN']
 })
-
-async function getAll(urlEndpoint) {
-  const objects = []
-  for await (const obj of getAllGenerator(urlEndpoint)) objects.push(obj)
-  return objects
-}
-
-async function* getAllGenerator(urlEndpoint) {
-  let next = urlEndpoint
-  while (true) {
-    const resp = await fetch(next, { 
-      headers: { Authorization: `token ${GITHUB_TOKEN}` }
-    })
-
-    if (!resp.ok) {
-      if (resp.headers.get('x-ratelimit-remaining') === '0') {
-        const resetTime = Math.round(resp.headers.get('x-ratelimit-reset') - Date.now() / 1000)
-        throw new Error(`Ratelimited. Resets in ${resetTime} seconds.`)
-      }
-      throw new Error(`${resp.status} ${resp.statusText}`)
-    }
-  
-    const json = await resp.json()
-    yield *json
-
-    if (!resp.headers.get('link')) break
-    const next_link = resp.headers.get('link').split(/,/)
-      .map(x => x.split(/;/))
-      .find(x => x[1].includes('next'))
-
-    if (!next_link) break
-    next = next_link[0].trim().slice(1, -1)
-  }
-}
-
-function linkifyPRs(msg) {
-  return msg.replace(/#(\d+)/g, (_, pr_id) =>
-    `<https://github.com/${ORGANIZATION_NAME}/${REPO_NAME}/pull/${pr_id}|#${pr_id}>`)
-}
-
-async function fetchUnreleasedCommits(branch) {
-  const tags = await getAll(`${GH_API_PREFIX}/repos/${ORGANIZATION_NAME}/${REPO_NAME}/tags`)
-  const unreleased = []
-  const url = `${GH_API_PREFIX}/repos/${ORGANIZATION_NAME}/${REPO_NAME}/commits?sha=${branch}`
-
-  for await (const commit of getAllGenerator(url)) {
-    const tag = tags.find(t => t.commit.sha === commit.sha)
-    if (tag) break
-    unreleased.push(commit)
-  }
-  return unreleased
-}
