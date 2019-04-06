eb390d3170bfa510b868528d873129e97b855801
Shelley Vohr <shelley.vohr@gmail.com>
2019-04-06 15:43:24 -0400

    refactor: clean up commit formatting code

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ app.post('/audit', async (req, res) => {
     if (commits.length === 0) {
       response = `*No unreleased commits on ${branch}*`
     } else {
-      const formattedCommits = commits.map(c => { 
-        return `- \`<${c.html_url}|${c.sha.slice(0, 8)}>\` ${linkifyPRs(c.commit.message.split(/[\r\n]/)[0])}` 
+      const formattedCommits = commits.map(c => {
+        const prLink = linkifyPRs(c.commit.message.split(/[\r\n]/)[0])
+        return `- \`<${c.html_url}|${c.sha.slice(0, 8)}>\` ${prLink}` 
       }).join('\n')
       response = `Unreleased commits in *${branch}*:\n${formattedCommits}`
     }
