435ee00a5f8bf04cf605494b4d08969cd488f237
Shelley Vohr <shelley.vohr@gmail.com>
2019-11-12 12:57:25 +0000

    chore: make it prettier

--- utils/commits-helpers.js
+++ utils/commits-helpers.js
@@ -1,55 +1,56 @@
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
-const { GITHUB_TOKEN } = require('../constants')
+const { GITHUB_TOKEN } = require('../constants');
 
 // Formulate a list of all commits based on a certain url endpoint
 // for a release tag
 async function getAll(urlEndpoint) {
-  const objects = []
-  for await (const obj of getAllGenerator(urlEndpoint)) objects.push(obj)
-  return objects
+  const objects = [];
+  for await (const obj of getAllGenerator(urlEndpoint)) objects.push(obj);
+  return objects;
 }
 
 // Generate and iterate through the JSON blob representing commits
 async function* getAllGenerator(urlEndpoint) {
-  let next = urlEndpoint
+  let next = urlEndpoint;
   while (next) {
     const resp = await fetch(next, {
-      headers: { Authorization: `token ${GITHUB_TOKEN}` }
-    })
+      headers: { Authorization: `token ${GITHUB_TOKEN}` },
+    });
 
     if (!resp.ok) {
       if (resp.headers.get('x-ratelimit-remaining') === '0') {
-        const resetTime = Math.ceil(resp.headers.get('x-ratelimit-reset') - Date.now() / 1000)
-        throw new Error(`Ratelimited. Resets in ${resetTime} seconds.`)
+        const resetTime = Math.ceil(resp.headers.get('x-ratelimit-reset') - Date.now() / 1000);
+        throw new Error(`Ratelimited. Resets in ${resetTime} seconds.`);
       }
-      throw new Error(`${resp.status} ${resp.statusText}`)
+      throw new Error(`${resp.status} ${resp.statusText}`);
     }
 
-    const json = await resp.json()
-    yield* json
+    const json = await resp.json();
+    yield* json;
 
-    if (!resp.headers.get('link')) break
+    if (!resp.headers.get('link')) break;
 
     // try to get the next link from the `link` header
-    // ex. this link: 
+    // ex. this link:
     // <https://api.github.com/repositories/9384267/tags?page=15>; rel="prev",
     // <https://api.github.com/repositories/9384267/tags?page=17>; rel="next",
     // <https://api.github.com/repositories/9384267/tags?page=18>; rel="last",
     // <https://api.github.com/repositories/9384267/tags?page=1>; rel="first"
     // would extract this next_link:
     // [ ' <https://api.github.com/repositories/9384267/tags?page=17>', ' rel="next"' ]
-    const next_link = resp.headers.get('link')
+    const next_link = resp.headers
+      .get('link')
       .split(/,/)
       .map(x => x.split(/;/))
-      .find(x => x[1].includes('next'))
+      .find(x => x[1].includes('next'));
 
-    if (!next_link) break
-    next = next_link[0].trim().slice(1, -1)
+    if (!next_link) break;
+    next = next_link[0].trim().slice(1, -1);
   }
 }
 
 module.exports = {
   getAll,
-  getAllGenerator
-}
+  getAllGenerator,
+};
