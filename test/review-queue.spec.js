ca1070db680cca3f2737511c3ac780c0a8427622
Shelley Vohr <shelley.vohr@gmail.com>
2020-11-23 09:51:37 -0800

    spec: fix review queue days-old failures

--- test/review-queue.spec.js
+++ test/review-queue.spec.js
@@ -7,9 +7,15 @@ describe('review-queue', () => {
     const prefix = 'api-review';
     const message = buildReviewQueueMessage(prefix, prs);
 
-    const expected = `* <https://github.com/electron/electron/pull/25198|#25198> - feat: implement allowFileAccess loadExtension option · _ChALkeR_ · _81 days old_
-* <https://github.com/electron/electron/pull/24849|#24849> - feat: add session.setCorsOriginAccessList API · _lishid_ · _105 days old_
-* <https://github.com/electron/electron/pull/23460|#23460> - [wip] feat: add hooks for profiling startup performance · _ckerr_ · _195 days old_
+    const days = prs.map(pr => {
+      return Math.round(
+        (+new Date() - +new Date(pr.created_at)) / (1000 * 60 * 60 * 24),
+      );
+    })
+
+    const expected = `* <https://github.com/electron/electron/pull/25198|#25198> - feat: implement allowFileAccess loadExtension option · _ChALkeR_ · _${days[0]} days old_
+* <https://github.com/electron/electron/pull/24849|#24849> - feat: add session.setCorsOriginAccessList API · _lishid_ · _${days[1]} days old_
+* <https://github.com/electron/electron/pull/23460|#23460> - [wip] feat: add hooks for profiling startup performance · _ckerr_ · _${days[2]} days old_
  *Found 3 open PRs with label \`api-review/requested 🗳\`*`;
 
     expect(message).to.equal(expected);
