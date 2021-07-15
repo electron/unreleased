20107aeb6b61e1c493afa012754b4caa06ce7bf3
George Xu <33054982+georgexu99@users.noreply.github.com>
2021-07-15 03:12:55 -0700

    chore: increase number of supported versions to reflect release cadence migration (#38)

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ const GH_API_PREFIX = 'https://api.github.com';
 const ORGANIZATION_NAME = process.env.ORGANIZATION_NAME || 'electron';
 const REPO_NAME = process.env.REPO_NAME || 'electron';
 
-const NUM_SUPPORTED_VERSIONS = process.env.NUM_SUPPORTED_VERSIONS || 4;
+const NUM_SUPPORTED_VERSIONS = process.env.NUM_SUPPORTED_VERSIONS || 5;
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
