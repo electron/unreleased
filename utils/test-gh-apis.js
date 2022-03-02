f8bea932b9476f9359f3fd28450c493ae0c64442
John Kleinschmidt <jkleinsc@github.com>
2022-03-01 16:18:56 -0800

    fixup for broken tests

--- utils/test-gh-apis.js
+++ utils/test-gh-apis.js
@@ -16,7 +16,7 @@ const argOptions = {
 
 const { argValues } = parseArgs(process.argv, argOptions);
 
-const branch = argValues?.branch || 'main';
+const branch = argValues?.branch || '17-x-y';
 const tag = argValues?.tag || 'v17.0.0';
 const prefix = argValues?.prefix || 'api-review';
 const author = argValues?.author || null;
@@ -28,35 +28,37 @@ async function testIt() {
     console.log(`Testing  getSemverForCommitRange for ${branch}`);
     const semverType = await getSemverForCommitRange(commits);
     console.log(
-      `SUCCESS getting getSemverForCommitRange; semver type for ${branch} is ${semverType}`,
+      `ok getting getSemverForCommitRange; semver type for ${branch} is ${semverType}`,
     );
     console.log(`Testing if ${tag} is draft`);
     const isDraft = await releaseIsDraft(tag);
     if (isDraft) {
-      console.log(`SUCCESS ${tag} is draft`);
+      console.log(`ok ${tag} is draft`);
     } else {
-      console.log(`SUCCESS ${tag} is NOT draft`);
+      console.log(`ok ${tag} is NOT draft`);
     }
     console.log(
       `Testing fetchNeedsManualPRs for ${branch} and ${author || 'any author'}`,
     );
     const manualPRS = await fetchNeedsManualPRs(branch, author);
     console.log(
-      `SUCCESS running fetchNeedsManualPRs for ${branch} and ${author ||
+      `ok running fetchNeedsManualPRs for ${branch} and ${author ||
         'any author'}; there are ${manualPRS.length} needing manual PRs`,
     );
     console.log(`Testing fetchReviewQueuePRs for ${prefix}`);
     const reviewQueuePRS = await fetchReviewQueuePRs(prefix);
     console.log(
-      `SUCCESS running fetchReviewQueuePR for ${prefix}; there are ${reviewQueuePRS.length} needing manual PRs`,
+      `ok running fetchReviewQueuePR for ${prefix}; there are ${reviewQueuePRS.length} needing manual PRs`,
     );
     console.log(`Testing fetchUnmergedPRs for ${branch}`);
     const unmergedPRS = await fetchUnmergedPRs(branch);
     console.log(
-      `SUCCESS running fetchReviewQueuePR for ${branch}; there are ${unmergedPRS.length} needing manual PRs`,
+      `ok running fetchReviewQueuePR for ${branch}; there are ${unmergedPRS.length} needing manual PRs`,
     );
   } catch (err) {
-    console.error(`Error testing GitHub API calls`);
+    console.error(`Error testing GitHub API calls`, err);
+    process.exit(1);
   }
+  process.exit();
 }
 testIt();
