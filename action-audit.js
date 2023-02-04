f24ad12fd7f3ae7f900d0b9de52a1615cd62a877
Shelley Vohr <shelley.vohr@gmail.com>
2023-02-04 10:33:39 +0100

    chore: remove review-queue logic from action-audit

---
+++ action-audit.js
@@ -0,0 +1,79 @@
+const core = require('@actions/core');
+const { WebClient } = require('@slack/web-api');
+
+const {
+  buildUnreleasedCommitsMessage,
+  fetchUnreleasedCommits,
+} = require('./utils/unreleased-commits');
+
+const {
+  buildNeedsManualPRsMessage,
+  fetchNeedsManualPRs,
+} = require('./utils/needs-manual-prs');
+
+const {
+  SLACK_BOT_TOKEN,
+  ACTION_TYPE,
+  AUDIT_POST_CHANNEL,
+} = require('./constants');
+
+const Actions = {
+  UNRELEASED: 'unreleased',
+  NEEDS_MANUAL: 'needs-manual',
+};
+
+const { getSupportedBranches } = require('./utils/helpers');
+
+const slackWebClient = new WebClient(SLACK_BOT_TOKEN);
+
+async function run() {
+  const initiatedBy = 'automatic audit';
+  const branches = await getSupportedBranches();
+  for (const branch of branches) {
+    core.info(`Auditing ${ACTION_TYPE} on branch ${branch}`);
+
+    let commits;
+    if (ACTION_TYPE === Actions.UNRELEASED) {
+      commits = (await fetchUnreleasedCommits(branch)).commits;
+    } else if (ACTION_TYPE === Actions.NEEDS_MANUAL) {
+      commits = await fetchNeedsManualPRs(branch, null /* author */);
+    }
+
+    core.info(`Found ${commits.length} commits on ${branch}`);
+    if (ACTION_TYPE === 'unreleased' && commits.length >= 10) {
+      core.info(
+        `Reached ${commits.length} commits on ${branch}, time to release.`,
+      );
+    }
+
+    let text = '';
+    if (ACTION_TYPE === Actions.UNRELEASED) {
+      text += buildUnreleasedCommitsMessage(branch, commits, initiatedBy);
+    } else if (ACTION_TYPE === Actions.NEEDS_MANUAL) {
+      text += buildNeedsManualPRsMessage(
+        branch,
+        commits,
+        true /* shouldRemind */,
+      );
+    }
+
+    const result = await slackWebClient.chat.postMessage({
+      channel: AUDIT_POST_CHANNEL,
+      unfurl_links: false,
+      text,
+    });
+
+    if (result.ok) {
+      core.info(`✅ Audit message sent for ${branch} 🚀`);
+    } else {
+      core.setFailed(
+        `❌ Unable to send audit info for ${branch}: ` + result.error,
+      );
+    }
+  }
+  core.info(` ✅ All release branches audited successfully`);
+}
+
+run().catch(e => {
+  core.setFailed(`Workflow failed: ${e.message}`);
+});
