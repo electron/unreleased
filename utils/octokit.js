1ff730d2fd167cbf819cadfa8f23439cffb369f6
David Sanders <dsanders11@ucsbalum.com>
2023-11-16 11:50:59 -0800

    chore: fallback to PAT/no auth if GitHub App auth not present

--- utils/octokit.js
+++ utils/octokit.js
@@ -13,15 +13,23 @@ let octokit;
 const getOctokit = async () => {
   if (octokit) return octokit;
 
-  const creds = appCredentialsFromString(UNRELEASED_GITHUB_APP_CREDS);
-  const authOpts = await getAuthOptionsForRepo(
-    {
-      owner: ORGANIZATION_NAME,
-      name: REPO_NAME,
-    },
-    creds,
-  );
-  octokit = new Octokit({ ...authOpts });
+  if (UNRELEASED_GITHUB_APP_CREDS) {
+    const creds = appCredentialsFromString(UNRELEASED_GITHUB_APP_CREDS);
+    const authOpts = await getAuthOptionsForRepo(
+      {
+        owner: ORGANIZATION_NAME,
+        name: REPO_NAME,
+      },
+      creds,
+    );
+    octokit = new Octokit({ ...authOpts });
+  } else if (process.env.GITHUB_TOKEN) {
+    octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    });
+  } else {
+    octokit = new Octokit();
+  }
 
   return octokit;
 };
